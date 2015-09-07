3affcadffc43434e5f763a7282dd51b97a3895b2
Liam Marshall <archimedespi3141@gmail.com>
2015-09-07 18:17:41 -0500

    Basic bot class

---
+++ espresso/bot.py
@@ -0,0 +1,40 @@
+import logging
+import time
+import re
+
+from slackclient import SlackClient
+
+class Espresso(object):
+    """The bot's main class.
+    Handles connections and all the things.
+    Delegates to plugin files.
+    """
+
+    def __init__(self, config):
+        self.config = config
+        self.api_token = config['api_token']
+        self.debug = config['debug']
+        self.slack_client = None
+
+    def connect(self):
+        """Helper method to connect to Slack.
+        Creates a new SlackClient with ``self.api_token``.
+        """
+
+        self.slack_client = SlackClient(self.api_token)
+        self.slack_client.rtm_connect() # connect to the real-time messaging system
+
+    def brew(self):
+        """Run the bot.
+        Starts an infinite processing loop.
+        """
+
+        logging.info("starting the bot")
+        self.connect()
+
+        while True:
+            for reply in self.slack_client.rtm_read():
+                print(reply)
+            # TODO: take loaded list of plugin callback regexes and check them, then call the callbacks
+            time.sleep(.1)
+

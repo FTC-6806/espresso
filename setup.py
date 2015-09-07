a958c8510a8baaeb149c9d723a3e28d251e537a0
Liam Marshall <archimedespi3141@gmail.com>
2015-09-07 10:36:08 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name = 'notebook_from_slack',
+	version = 1.0,
+	author = 'Liam Marshall',
+	author_email = 'liam@cpan.org',
+	url = 'https://github.com/ftc-6806/notebook_from_slack'
+	)

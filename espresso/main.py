16066e7684352b879dafcf4e2783a11542072e2e
Liam Marshall <archimedespi3141@gmail.com>
2015-09-07 18:15:59 -0500

    config and logging

--- espresso/main.py
+++ espresso/main.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import yaml
+import logging
 
 def parse_args():
     parser = ArgumentParser()
@@ -13,6 +14,9 @@ def parse_args():
 
 def main():
     args = parse_args()
+    config = yaml.load(file(args.config or 'botconfig.yaml', 'r'))
+    logging.basicConfig(level=getattr(logging, config['logging']['level']), format="%(levelname)s from %(filename)s at %(asctime)s | %(message)s")
+    logging.debug(config)
 
 if __name__ == '__main__':
     main()

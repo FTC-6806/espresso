8b59fdacaeb693d14a22ba749007284939519e4a
Liam Marshall <archimedespi3141@gmail.com>
2015-09-10 09:38:45 -0500

    Dump announcements to the DB

--- plugins/notebook.py
+++ plugins/notebook.py
@@ -1,11 +1,19 @@
 # the notebook plugin
 # handles notebook creation
 
+import dateutil.parser
 from espresso.main import robot
+from docx import Document
 
-@robot.hear('(?i)Announcement for (?P<date>[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]): (?P<announcement>.*)')
+
+@robot.hear('(?i)Announcement for (?P<date>[0-9]?[0-9]\/[0-9]?[0-9]\/[0-9][0-9]): (?P<announcement>.*)')
 def got_announcement(res):
-    date = res.match.group('date')
+    date = dateutil.parser.parse(res.match.group('date'))
     announcement = res.match.group('announcement')
+    user = res.msg.user.name
 
     res.reply(res.msg.user, "Got Announcement for date {}: {}".format(date, announcement))
+    res.robot.brain.db.insert({"plugin": "notebook", "type": "announcement",
+        "date": date.isoformat(), "announcement": announcement,
+        "user": user, "channel": res.msg.channel.name})
+

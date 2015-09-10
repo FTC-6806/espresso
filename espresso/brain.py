34d3ed5d9c626ffd96dcb97407ea9737c1cb7306
Liam Marshall <archimedespi3141@gmail.com>
2015-09-10 09:38:02 -0500

    Remove docstring from brain because it's inaccurate.

--- espresso/brain.py
+++ espresso/brain.py
@@ -1,11 +1,5 @@
 from tinydb import TinyDB, where
 
 class Brain(object):
-    """The robot's brain.
-    Uses TinyDB to store stuff into a simple, flat-json-file on-disk db.
-    If you want to Espresso on Heroku or any other diskless PaaS,
-    you can wire in MongoDB or something instead.
-    """
-
     def __init__(self, db_location):
         self.db = TinyDB(db_location)

2d0dcc03ec075689c04c5a1ba3e50ac70f9eb2d3
Liam Marshall <archimedespi3141@gmail.com>
2015-09-17 14:45:30 -0500

    :shirt: stylistic things!

--- espresso/brain.py
+++ espresso/brain.py
@@ -1,5 +1,6 @@
 from tinydb import TinyDB
 
+
 class Brain(object):
     def __init__(self, db_location):
         self.db = TinyDB(db_location)
--- espresso/main.py
+++ espresso/main.py
@@ -4,7 +4,9 @@ import yaml
 from bot import Espresso
 
 config = yaml.load(file('botconfig.yaml', 'r'))
-logging.basicConfig(level=getattr(logging, config['logging']['level']), format="%(levelname)s from %(filename)s at %(asctime)s | %(message)s")
-logging.debug(config)
+
+logging.basicConfig(level=getattr(logging, config['logging']['level']),
+    format="%(levelname)s from %(filename)s at %(asctime)s | %(message)s")
+logging.debug("config is %s", config)
 
 robot = Espresso(config)

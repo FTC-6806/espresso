50e4d8bffa2019902ff7c4983efb427562ce1dbf
Liam Marshall <archimedespi3141@gmail.com>
2015-09-17 14:50:19 -0500

    :shirt: visual indentation

--- espresso/main.py
+++ espresso/main.py
@@ -6,7 +6,7 @@ from bot import Espresso
 config = yaml.load(file('botconfig.yaml', 'r'))
 
 logging.basicConfig(level=getattr(logging, config['logging']['level']),
-    format="%(levelname)s from %(filename)s at %(asctime)s | %(message)s")
+                    format="%(levelname)s from %(filename)s at %(asctime)s | %(message)s")
 logging.debug("config is %s", config)
 
 robot = Espresso(config)

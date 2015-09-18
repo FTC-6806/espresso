0031d61650830b0f45ca20759c0f2805bd59853e
Liam Marshall <archimedespi3141@gmail.com>
2015-09-17 19:18:34 -0500

    Remove uid for robot

--- espresso/bot.py
+++ espresso/bot.py
@@ -73,10 +73,8 @@ class Espresso(object):
                 logging.debug("Raw message: %s", msg)
                 if 'type' in msg:
                     if msg['type'] == 'message' and 'subtype' not in msg:
-                        message = Message(
-                                          User(msg['user'],
-                                               self.slack_client.server.users.find(msg['user']).name
-                                               ),
+                        message = Message(User(msg['user'],
+                                               self.slack_client.server.users.find(msg['user']).name),
                                           self.slack_client.server.channels.find(msg['channel']),
                                           msg['text'])
                         for listener in self.listeners:
@@ -89,7 +87,7 @@ class Espresso(object):
         if ltype == ListenerType.heard:
             self.listeners.append(Listener(self, regex, function))
         elif ltype == ListenerType.heard_with_name:
-            regex = "^(?:\<\@U0A9396LC\>|{})\s*:?\s*".format(self.user.name) + regex
+            regex = "^(?:\<\@{uid}\>|{uname})\s*:?\s*".format(uid=self.user.uid, uname=self.user.name) + regex
             self.listeners.append(Listener(self, regex, function))
 
         logging.debug("Added listener of type %s with regex %s calling %s",

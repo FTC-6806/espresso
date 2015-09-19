15bcfd832c992a12fd607e267f313932acf59d63
Liam Marshall <archimedespi3141@gmail.com>
2015-09-18 22:40:53 -0500

    Add a bunch of commenting

--- plugins/notebook.py
+++ plugins/notebook.py
@@ -11,37 +11,55 @@ from docx import Document
 from espresso.main import robot
 from tinydb import where
 
+# the regex used to identify an Announcement message
 ANNOUNCEMENT_REGEX = r'(?is)Announcement for (?P<date>\d+/\d+/\d+): (?P<announcement>.*)'
 
-
+# listen for an announcement
 @robot.hear(ANNOUNCEMENT_REGEX)
 def got_announcement(res):
+    # grab out a bunch of things from the regex to add to the db
     date = dateutil.parser.parse(res.match.group('date'))
     announcement = res.match.group('announcement')
     user = res.msg.user.name
 
+    # insert everything into the db
+    # TODO: build a new brain api and refactor?
     logging.debug("Got Announcement for date %s: %s", date, announcement)
     res.robot.brain.db.insert({"plugin": "notebook", "type": "announcement",
         "date": date.isoformat(), "announcement": announcement,
         "user": user, "channel": res.msg.channel.name})
 
+# backfill responder
 @robot.respond(r'(?i)backfill announcements for (?P<date>\d+/\d+/\d+)')
 def backfill_announcements(res):
+    # pull out the target date from the backfill command
     target_date = dateutil.parser.parse(res.match.group('date'))
 
+    # grab the entire history of #announcements (rather the last 100 messages) from Slack's api
+    # this is a raw api call. refactor?
     channel_history = json.loads(res.robot.slack_client.api_call('channels.history',
         channel=res.robot.slack_client.server.channels.find("announcements").id,
         inclusive=1))
+    # grab the messages from the history dict
     channel_message_type_events = channel_history['messages']
+    # filter out only actual messages, not other subtypes
     channel_messages = filter(lambda m: ((m.get('type') == 'message') and ('subtype' not in m)), channel_message_type_events)
 
+    # for every message in the history array
     for m in channel_messages:
+        # check if it matches the announcement regex
         match = re.search(ANNOUNCEMENT_REGEX, m['text'])
         if match:
+            # pull out a bunch of regex groups
             date = dateutil.parser.parse(match.group('date'))
             announcement = match.group('announcement')
+
+            # grab the user's name from slackclient's user dict
             user = res.robot.slack_client.server.users.find(m['user']).name
+
+            # if the date is the one the original backfill command specified
             if date == target_date:
+                # insert the announcement into the db
                 logging.debug("Got Announcement for date %s by user %s: %s",
                               date, user, announcement)
                 res.robot.brain.db.insert({"plugin": "notebook", "type": "announcement",
@@ -49,7 +67,7 @@ def backfill_announcements(res):
                     "user": user, "channel": 'announcements'})
 
 
-
+# notebook creation responder
 @robot.respond(r'(?i)make a (new )?notebook (entry|template) for (?P<date>\d+/\d+/\d+)')
 def make_entry(res):
     date = dateutil.parser.parse(res.match.group('date'))

0928407c930fa4dd7746055a414bcb2035f71897
Liam Marshall <liam@cpan.org>
2015-09-20 19:40:08 -0500

    Don't import daemon unless selected in config

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
-import daemon
-
 from espresso.main import config
 from espresso.main import robot
 
 if 'daemonized' in config:
     if config['daemonized']:
+    	import daemon
         with daemon.DaemonContext():
             robot.brew()
 

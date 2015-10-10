82075e7dfc31e35b4f809bcfda0f0d27aecff70a
Liam Marshall <liam@cpan.org>
2015-10-09 22:51:34 -0500

    Add new SlackChatAdaptor

---
+++ espresso/chat_services/slack.py
@@ -0,0 +1,30 @@
+import json
+
+from slackclient import SlackClient
+from ..user import User
+
+class SlackChatAdaptor(object):
+	def __init__(self, config):
+		self.config = config
+		self.api_token = config['api_token']
+		self._bot_user = None
+		self.slack_client = None
+
+	def connect(self):
+		self.slack_client = SlackClient(self.api_token)
+		self.slack_client.rtm_connect()
+
+		# test auth and grab the current user
+		slack_test = json.loads(self.slack_client.api_call('auth.test'))
+		self._bot_user = User(slack_test['user_id'], slack_test['user'])
+
+	@property
+	def bot_user(self):
+	    return self._bot_user
+
+	def read_stream(self):
+		return self.slack_client.rtm_read()
+
+	def send(self, message, channel):
+		"""Send a message to slack"""
+		self.slack_client.server.channels.find(channel).send_message(message)
